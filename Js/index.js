0dbe5a5b2e4379517e35fd49ad01682abf770492
Alice Alfaioli <alicealice5693@yahoo.it>
2024-01-31 16:39:55 +0100

    first commit

---
+++ Js/index.js
@@ -0,0 +1,11 @@
+const handleSubmit = function (e) {
+  e.preventDefaul();
+  add();
+  attachComplete();
+};
+
+const add = function () {
+  let tasks = document.querySelector("#tasks");
+  let inputField = document.querySelector("#newTask input");
+  let newTask = ` `;
+};
